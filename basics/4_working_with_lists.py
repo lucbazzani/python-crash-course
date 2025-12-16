95f515add9c198fe5ccd8e26af02ba11d589240b
Lucas Bazzani <lu.bazzani@hotmail.com>
2025-12-16 19:31:29 -0300

    Add new exercises for counting, summing, and generating number lists (from 4-3 to 4-9)

--- basics/4_working_with_lists.py
+++ basics/4_working_with_lists.py
@@ -32,3 +32,48 @@ for animal in animals:
     print(f"{animal.title()} is a wild bird.")
 
 print("All of these animals are brazilian birds.")
+
+# 4-3. Counting to Twenty: Use a for loop to print the numbers from 1 to
+# 20, inclusive.
+print('\n### EXERCISE 4-3 ###')
+for number in range(1, 21):
+    print(number)
+
+# 4-4. One Million: Make a list of the numbers from one to one million, and
+# then use a for loop to print the numbers. (If the output is taking too long,
+# stop it by pressing CTRL-C or by closing the output window.)
+# print('\n### EXERCISE 4-4 ###')
+# numbers = list(range(1, 1000001))
+# for i in numbers:
+#     print(i)
+
+# 4-5. Summing a Million: Make a list of the numbers from one to one
+# million, and then use min() and max() to make sure your list actually starts at
+# one and ends at one million. Also, use the sum() function to see how quickly
+# Python can add a million numbers.
+print('\n### EXERCISE 4-5 ###')
+one_to_one_million = list(range(1, 1000001))
+print(f'min: {min(one_to_one_million)}')
+print(f'max: {max(one_to_one_million)}')
+print(f'sum: {sum(one_to_one_million)}')
+
+# 4-6. Odd Numbers: Use the third argument of the range() function to make
+# a list of the odd numbers from 1 to 20. Use a for loop to print each number.
+print('\n### EXERCISE 4-6 ###')
+odd_numbers = list(range(1, 20, 2))
+for odd in odd_numbers:
+    print(odd)
+
+# 4-7. Threes: Make a list of the multiples of 3, from 3 to 30. Use a for loop to
+# print the numbers in your list
+print('\n### EXERCISE 4-7 ###')
+multiples_of_three = list(range(3, 31, 3))
+for multiple in multiples_of_three:
+    print(multiple)
+
+# 4-9. Cube Comprehension: Use a list comprehension to generate a list of
+# the first 10 cubes.
+print('\n### EXERCISE 4-9 ###')
+cubes = [value**3 for value in range(1, 11)]
+for cube in cubes:
+    print(cube)
