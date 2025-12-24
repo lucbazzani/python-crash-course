d1e09aba9bda9ecd56946d2c7d658a6006c8dc76
Lucas Bazzani <lu.bazzani@hotmail.com>
2025-12-24 19:52:39 -0300

    Add functions exercise 8-15

---
+++ basics/modules_exercises_chapter_8/8-15_printing_models.py
@@ -0,0 +1,10 @@
+import printing_functions as pf
+
+# 8-15. Printing Models: Put the functions for the example
+# printing_models.py in a separate file called printing_functions.py. Write an
+# import statement at the top of printing_models.py, and modify the file to use
+# the imported functions.
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron'] 
+completed_models = [] 
+
+pf.print_models(unprinted_designs, completed_models)
