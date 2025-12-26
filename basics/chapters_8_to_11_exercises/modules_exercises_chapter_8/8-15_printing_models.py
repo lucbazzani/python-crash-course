67abf10154d73a7767df8453cc271ace9babbbbc
Lucas Bazzani <lu.bazzani@hotmail.com>
2025-12-26 15:17:30 -0300

    Refactor: Organize exercise files into folders

---
+++ basics/chapters_8_to_11_exercises/modules_exercises_chapter_8/8-15_printing_models.py
@@ -0,0 +1,10 @@
+import printing_functions as pf
+
+# 8-15. Printing Models: Put the functions for the examplunctions.py. Write an
+# import statement at the top of printing_models.py, and e
+# printing_models.py in a separate file called printing_fmodify the file to use
+# the imported functions.
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron'] 
+completed_models = [] 
+
+pf.print_models(unprinted_designs, completed_models)
