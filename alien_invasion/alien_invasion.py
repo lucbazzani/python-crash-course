a870de207d4c69116b6ed5a17baa6bcdf97eb48e
Lucas Bazzani <lu.bazzani@hotmail.com>
2026-01-01 18:53:28 -0300

    Refactor Alien Invasion game: Introduce Settings class for configuration and update screen initialization

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,17 +2,30 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
     
     def __init__(self):
         """Initialize the game, and create game resources."""
     
+        # initialize all the imported pygame modules
         pygame.init()
+        
+        # make an instance of the class Clock for frame rate management
+        self.clock = pygame.time.Clock()
+
+        # make an instace of the class Settings with the default settings data
+        self.settings = Settings()
 
         # create a display window (surface) and assign it to self.screen
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color.
+        self.bg_color = (0, 25, 50)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -28,8 +41,16 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # fill the screen with the background color during each pass 
+            # through the loop.
+            self.screen.fill(self.settings.bg_color)
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+            # The tick() method takes the value of 60 in the argument so Pygame 
+            # will do its best to make the loop run exactly 60 times per second.
+            self.clock.tick(self.settings.frame_rate)
 
 
 # only runs if the file is called directly
---
+++ alien_invasion/settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+
+        # Frame rate settings
+        self.frame_rate = 60
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 25, 50)
