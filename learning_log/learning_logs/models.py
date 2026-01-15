64b3d28a55e6eb77cf01e4f58e7a1818cd90d695
Lucas Bazzani <lu.bazzani@hotmail.com>
2026-01-14 21:13:44 -0300

    Add Entry model and register it in admin

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -9,3 +9,22 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    # The 'Meta' class holds extra information for managing a model; here, 
+    # it lets us set a special attribute telling Django to use 'Entries' when it 
+    # needs to refer to more than one entry. 
+    # Without this, Django would refer to multiple entries as 'Entrys'.
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a simple string representing the entry.""" 
+        
+        return f"{self.text[:50]}[...]"
