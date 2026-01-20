f2966bbe70e3fbfeeb07d034238a2947860539dc
Lucas Bazzani <lu.bazzani@hotmail.com>
2026-01-20 20:07:07 -0300

    Add edit entry functionality with corresponding view, template, and URL

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
     
     # Page for adding a new entry
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # Page for editing an entry
+    # the value of id is assigned to the parameter entry_id. Django sends 
+    # requests that match this format to the view function edit_entry()
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),    
 ]
